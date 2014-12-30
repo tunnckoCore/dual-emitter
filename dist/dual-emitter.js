dc0252ee043c01094cd6a8f2923f4990e4312132
tunnckoCore <mameto_100@mail.bg>
2014-12-30 08:29:39 +0200

    add it to dist/ (will live there only if stable)

---
+++ dist/dual-emitter.js
@@ -0,0 +1,58 @@
+/**
+ * dual-emitter <https://github.com/tunnckoCore/dual-emitter>
+ *
+ * Copyright (c) 2014 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+(function(global, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(factory());
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    global.DualEmitter = factory();
+  }
+})(this, function() {
+  'use strict';
+
+  function DualEmitter($) {
+    $ = $ || this;
+    $.events = $.events || {};
+    $.hasEvent = function hasEvent(name) {
+      return (name in $.events) ? $.events[name] : 0;
+    };
+    $.isDom = function isDom(obj) {
+      obj = Object.prototype.toString.call(obj).slice(8, -1);
+      return /HTML(?:.*)Element/.test(obj);
+    }
+    $.on = function on(name, fn, func) {
+      if (typeof name !== 'string' || $.isDom(name)) {
+        if (name.attachListener) {
+          name.attachListener('on' + fn, func, false);
+          return $;
+        }
+        name.addEventListener(fn, func, false);
+
+        $.events['i:' + fn] = func;
+        return $;
+      }
+      $.events[name] = fn;
+      return $;
+    };
+    $.off = function off(name) {
+      if (!$.hasEvent(name)) {delete $.events[name];}
+      return $;
+    };
+    $.emit = function emit(args) {
+      args = [].slice.call(arguments);
+      args.shift().split(' ').forEach(function(e) {
+        $.hasEvent(e).apply(this, args);
+      });
+      return $;
+    };
+    return $;
+  }
+
+  return DualEmitter;
+});
